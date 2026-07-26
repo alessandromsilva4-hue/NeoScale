32bccf383ffaad8c2f6a9fa1d10800569b8afe7f
Alessandro Silva <alessandromsilva4@gmail.com>
2026-07-26 19:01:45 -0300

    Corrige carregamento do assistente de voz

--- js/iniciar-neoscale.js
+++ js/iniciar-neoscale.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 const path = require("path");
 
-const root = __dirname;
+const root = path.resolve(__dirname, "..");
 const port = 4180;
 const contentTypes = {
   ".css": "text/css; charset=utf-8",
